c9baa3ef262c0c01d56767f124a5d7bfa279ba56
Tom Hinkle <tmhinkle@gmail.com>
2022-02-13 11:23:57 +0000

    Organizing (moved get words code to its own file)

--- app/wordle/get_words.ts
+++ app/wordle/get_words.ts
@@ -1,14 +1,20 @@
 import {words} from './words';
 
-const startDate = new Date(2022,0,27);
+let today = new Date()
+let dateKey = today.getDate() * 30 + today.getFullYear() + today.getMonth() * 12;
 
-function getWord() {
-  let now = new Date().getTime()
-  let distance = now - startDate.getTime();
-  let daysPassed = Math.floor(distance / (1000 * 60 * 60 * 24));
-  return words[daysPassed % words.length];  
-}
-
-function testGetWord () {
-  console.log(getWord())
+console.log(today,'=>',dateKey);
+let salt = 54;
+let salt2 = 17;
+export function getTargetWords (n) {
+  let index = ((salt2 * n) + dateKey) % words.length;
+  let list = [words[index]];
+  for (let i=1; i<n; i++) {
+    let idx = (index + i*(n*salt)) % words.length;
+    while (list.indexOf(words[idx]) > -1) {
+      idx += 1;
+    }
+    list.push(words[idx])
+  }
+  return list;
 }
--- app/wordle/index.ts
+++ app/wordle/index.ts
@@ -2,22 +2,5 @@ export {checkWordle} from './validate';
 export let wordSize = 5;
 export {isWord,allWords} from './dictionary';
 import {words} from './words';
+export {getTargetWords} from './get_words';
 
-let today = new Date()
-let dateKey = today.getDate() * 30 + today.getFullYear() + today.getMonth() * 12;
-
-console.log(today,'=>',dateKey);
-let salt = 54;
-let salt2 = 17;
-export function getTargetWords (n) {
-  let index = ((salt2 * n) + dateKey) % words.length;
-  let list = [words[index]];
-  for (let i=1; i<n; i++) {
-    let idx = (index + i*(n*salt)) % words.length;
-    while (list.indexOf(words[idx]) > -1) {
-      idx += 1;
-    }
-    list.push(words[idx])
-  }
-  return list;
-}
