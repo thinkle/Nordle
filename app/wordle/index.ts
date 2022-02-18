e14ba56e8e8b592ee0eeb65136edb7715a44ddca
Tom Hinkle <tmhinkle@gmail.com>
2022-02-18 04:11:47 +0000

    Fix allWords fix

--- app/wordle/index.ts
+++ app/wordle/index.ts
@@ -1,6 +1,6 @@
 export {checkWordle} from './validate';
 export let wordSize = 5;
-export {isWord,allWords} from './dictionary';
+import {isWord,allWords} from './dictionary';
 import {words} from './words';
 export {getTargetWords, setToday} from './get_words';
 export {a11ySquares} from './a11y'
@@ -8,3 +8,5 @@ export {a11ySquares} from './a11y'
 for (let w of words) {
   allWords.push(w);
 }
+export {allWords, isWord};
+
