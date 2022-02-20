51308b2f3c4cf610349773e3f3a71a0a1f4b56f7
Tom Hinkle <tmhinkle@gmail.com>
2022-02-20 10:16:15 -0500

    vite config with static directory

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
-export default {
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  publicDir: "./static",
   server: {
-    host: '0.0.0.0',
-    hmr: {
+    host: "0.0.0.0",
+    /*hmr: {
       port: 443,
-    }
-  }
-}
+      host: "localhost",
+    },*/
+  },
+});
