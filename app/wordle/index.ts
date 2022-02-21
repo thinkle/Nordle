945ac0965c6ab4d3b3935a9d40af2933164fdfd9
Tom Hinkle <tmhinkle@gmail.com>
2022-02-21 00:51:20 -0500

    prettier

--- app/wordle/index.ts
+++ app/wordle/index.ts
@@ -1,11 +1,13 @@
 export { checkWordle, checkGuesses } from "./validate";
-export { wordSize } from './constants';
+export { wordSize } from "./constants";
 import { isWord, allWords } from "./dictionary";
 import { words } from "./words";
 export { getTargetWords, setToday, getDateKey } from "./get_words";
 export { a11ySquares } from "./a11y";
-
 for (let w of words) {
   allWords.push(w);
 }
 export { allWords, isWord };
+export let CORRECT = "🟩";
+export let PRESENT = "🟨";
+export let WRONG = "⬛";
--- app/wordle/validate.ts
+++ app/wordle/validate.ts
@@ -1,6 +1,6 @@
-import {wordSize} from './constants';
+import { wordSize } from "./constants";
 
-function countLetter(word, letter) {
+export function countLetter(word, letter) {
   let count = 0;
   for (let ltr of word) {
     if (ltr == letter) {
@@ -10,19 +10,19 @@ function countLetter(word, letter) {
   return count;
 }
 
-let victoryString = ""
-for (let i=0; i<wordSize; i++) {
-  victoryString += "🟩"
+let victoryString = "";
+for (let i = 0; i < wordSize; i++) {
+  victoryString += "🟩";
 }
 
 /**
  * Check guesses against word. Return which guess
  * was correct (1-based indexing).
  */
-export function checkGuesses (guesses : string[], word : string) : number {
+export function checkGuesses(guesses: string[], word: string): number {
   let i = 1; // count
   for (let g of guesses) {
-    let result = checkWordle(g, word).join('');
+    let result = checkWordle(g, word).join("");
     if (result == victoryString) {
       return i;
     }
